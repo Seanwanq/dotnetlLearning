b970d47d588557c3c4f2a2d51a9bc65164f0e0fa
seanwang <seanwang313@outlook.com>
2021-02-25 14:28:08 +0800

    Feb 25

---
+++ CSArray/ex_ArrayList/Program.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections.Generic;
+using System.Collections;
+using System;
+
+namespace ex_ArrayList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // ArrayList list = new ArrayList() { "aaa", "bbb", "abc", 123, 456 };
+
+            // int index = list.IndexOf("abc");
+            // if(index != -1)
+            // {
+            //     System.Console.WriteLine("集合中存在 abc 元素！");
+            // }
+            // else
+            // {
+            //     System.Console.WriteLine("集合中不存在 abc 元素！");
+            // }
+
+            // --------------
+
+            // ArrayList newList = new ArrayList();
+            // for (int i = 0; i < list.Count; i += 2)
+            // {
+            //     newList.Add(list[i]);
+            // }
+            // foreach(var v in newList)
+            // {
+            //     System.Console.WriteLine(v);
+            // }
+
+            // --------------
+
+            // ArrayList insertList = new ArrayList() { "A", "B", "C" };
+            // list.InsertRange(1, insertList);
+            // foreach(var v in list)
+            // {
+            //     System.Console.WriteLine(v);
+            // }
+
+            // --------------
+
+            // ArrayList list = new ArrayList() { "aaa", "bbb", "abc" };
+            // list.Sort();
+            // foreach(var v in list)
+            // {
+            //     System.Console.WriteLine(v);
+            // }
+
+            // --------------
+
+            ArrayList list = new ArrayList() { "a", "b", "c", 1, 2 };
+            MyCompare myCompare = new MyCompare();
+            list.Sort(myCompare);
+            foreach(var v in list)
+            {
+                System.Console.WriteLine(v);
+            }
+        }
+    }
+    class MyCompare:IComparer
+    {
+        public int Compare(object x, object y)
+        {
+            string str1 = x.ToString();
+            string str2 = y.ToString();
+            return str1.CompareTo(str2);
+        }
+    }
+}
---
+++ CSArray/ex_Queue/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ex_Queue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
